5b8673d45487233819c826fc6eb2c61b4f788496
sattaya037 <sattaya.ra@ics-th.com>
2020-11-17 15:24:13 +0700

    add line liff

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,9 @@ const Layout = (props) => {
             <Head>
                 <title>Hello Next.js</title>
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
+                <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
+               
+
             </Head>
             <Navbar />
             <div className="container">
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,45 @@ import Layout from '../components/layout';
 import fetch from 'isomorphic-unfetch';
 import { Card,CardDeck ,CardColumns    } from 'react-bootstrap';
 
+if (typeof window !== "undefined") {
 
+    liff
+        .init({
+            liffId: "1653935174-baeNzNDB" // Use own liffId
+        })
+        .then(() => {
+            // Start to use liff's api
+            if (!liff.isLoggedIn()) {
+                liff.login();
+              }
+        })
+        .catch((err) => {
+            // Error happens during initialization
+            console.log(err.code, err.message);
+        });
+
+
+  }
+
+
+
+
+
+// const liff = window.liff;
 const handleClick = (e) =>  {
-    console.log(e);
-    debugger
+    liff.getProfile()
+        .then(profile => {
+        const name = profile.displayName;
+        alert(name)
+        })
+        .catch((err) => {
+        console.log('error', err);
+        });
 }
 
+
 const Index = ({musicData}) => {
-    console.log(musicData)
+
     return (
         <Layout>
             <div>
@@ -20,8 +51,8 @@ const Index = ({musicData}) => {
                 {musicData.map((item, i) => {
                    return (
                    <Card  key={i}  onClick={handleClick}>
-                       {/* <Card.Img  src={i}>{item.poster_url}</Card.Img> */}
-                       <Card.Img variant="top" src={item.poster_url} />
+                       {/* <Card.Img   src={item.poster_url} */}
+                       <Card.Img variant="top" />
                         <Card.Title>{item.title}</Card.Title>
                         <Card.Text>{item.intro}</Card.Text>
                         <Card.Footer><small className="text-muted">{item.time_released}</small>  </Card.Footer>
@@ -40,6 +71,22 @@ const Index = ({musicData}) => {
 Index.getInitialProps = async function() {
     const response = await fetch(`https://www.what-song.com/api/recent-movies`);
     const result = await response.json();
+    // liff.init({ liffId: '1653935174-baeNzNDB' });
+    
+    // await liff.init({ liffId: '1653935174-baeNzNDB' }).catch(err=>{throw err});
+    // if (liff.isLoggedIn()) {
+    //   let getProfile = await liff.getProfile();
+    //   this.setState({
+    //     name: getProfile.displayName,
+    //     userLineID: getProfile.userId,
+    //     pictureUrl: getProfile.pictureUrl,
+    //   });
+    // }else{
+    //   liff.login();
+    // }
+
     return { musicData: result.data }
 }
+
+
 export default Index;
