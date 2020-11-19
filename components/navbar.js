05dfc612cb36270feee623bb3fb887fe5afdca50
sattaya037 <sattaya.ra@ics-th.com>
2020-11-19 14:38:25 +0700

    add UI

--- components/navbar.js
+++ components/navbar.js
@@ -2,57 +2,35 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import {Grid,GridList,GridListTile,GridListTileBar,Avatar} from '@material-ui/core';
+import login from './linelogin'
+// async function login() {
+//     if (process.browser) {
+//         const liff = window.liff;
+//         await liff.init({ liffId: '1653935174-baeNzNDB' }).catch(err=>{throw err});
+//         if (liff.isLoggedIn()) {
+//           let getProfile = await liff.getProfile();
+//             console.log(getProfile);
+//             return getProfile;
+//         }else{
+//           liff.login();
+//         }
+//     }    
 
-async function login() {
-    if (process.browser) {
-        const liff = window.liff;
-        await liff.init({ liffId: '1653935174-baeNzNDB' }).catch(err=>{throw err});
-        if (liff.isLoggedIn()) {
-          let getProfile = await liff.getProfile();
-            console.log(getProfile);
-            return getProfile;
-        }else{
-          liff.login();
-        }
-
-        // const liff = window.liff;
-        // liff
-        //     .init({
-        //         liffId: "1653935174-baeNzNDB" // Use own liffId
-        //     })
-        //     .then(() => {
-        //         // Start to use liff's api
-        //         if (!liff.isLoggedIn()) {
-        //             liff.login();
-    
-        //           }else{
-        //             a = "hello";
-    
-        //           }
-        //     })
-        //     .catch((err) => {
-        //         console.log(err.code, err.message);
-        //     });
-    }    
-
-}
-
-
+// }
+console.log(login())
 const SomeComponent = () => {
- 
-    var getProfile = login();
-    console.log(login())
 
-return <h1>{getProfile.displayName}</h1>
+    return <h1>Hello</h1>
   }
 
+
+
 const Navbar = ()  => {
-    console.log(login());
-    var a =login();
+
     return(
         <div>
             <ul>
-                <li><Link href="/"><a>{a}</a></Link></li>
+                <li><Link href="/"><a>Home</a></Link></li>
                 <li><Link href="/about"><a>About</a></Link></li>
             </ul>
 <style jsx>{`
@@ -71,21 +49,12 @@ ul li a {
                     text-decoration: none;
                 }
             `}</style>
-                {/* <SomeComponent /> */}
+                <SomeComponent />
 
 
         </div>
     );
 }
-
-
-
-    if (typeof window !== "undefined") {
-        var getheader =document.getElementById("header");
-            // console.log(getheader)
-            // getheader.innerHTML ="<Avatar alt='Remy Sharp' src='' /> "
-
-    }
 
 
 
